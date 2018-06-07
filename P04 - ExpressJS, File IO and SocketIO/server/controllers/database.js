346d078d3fb56100049d4cd7cfd7ebfbe598472d
Qin Yong <172900H@mymail.nyp.edu.sg>
2018-06-07 10:33:02 +0800

    Commit prac 4

---
+++ P04 - ExpressJS, File IO and SocketIO/server/controllers/database.js
@@ -0,0 +1,31 @@
+var Sequelize = require('sequelize');
+var sequelizeTransforms = require('sequelize-transforms');
+
+const sequelize =new Sequelize({
+    dialect: 'mssql',
+    dialectModulePath: 'tedious',
+    dialectOptions: {
+      driver: 'SQL Server Native Client 11.0',
+      instanceName: 'SQLEXPRESS03'
+    },
+    host: 'localhost',
+    username: 'p4',
+    password: 'p4pw',
+    database: 'p4db',
+    pool: {
+        min: 0,
+        max: 10,
+        idle: 10000
+      }
+  });
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+sequelizeTransforms(sequelize);
+
+module.exports.sequelize = sequelize;
+module.exports.Sequelize = Sequelize;
