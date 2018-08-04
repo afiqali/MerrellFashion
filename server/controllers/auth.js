8e94da1c96410c59de6e62440aada3f2db1f8be3
Joanne <170801C@mymail.nyp.edu.sg>
2018-08-04 15:52:31 +0800

    Sub

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,37 +1,37 @@
-// get gravatar icon from email
+// Import modules
 var gravatar = require('gravatar');
 var passport = require('passport');
 
-// Signin GET
+// Render Login page
 exports.signin = function(req, res) {
     // List all Users and sort by Date
     res.render('login', { title: 'Login Page', message: req.flash('loginMessage') });
 };
 
-// Signup GET
+// Render Signup page
 exports.signup = function(req, res) {
     // List all Users and sort by Date
     res.render('signup', { title: 'Signup Page', message: req.flash('signupMessage') });
 
 };
 
-// Profile GET
-exports.profile = function(req, res) {
-    // List all Users and sort by Date
-    res.render('profile', { title: 'Profile Page', user : req.user, avatar: gravatar.url(req.user.email ,  {s: '100', r: 'x', d: 'retro'}, true) });
-};
-
-// Logout function
+// Logout to Index page
 exports.logout = function () {
     req.logout();
     res.redirect('/');
 };
 
-// check if user is logged in
+// Check if user is logged in: Yes - continue, No - redirect to Login page
 exports.isLoggedIn = function(req, res, next) {
     if (req.isAuthenticated())
         return next();
     res.redirect('/login');
+};
+
+// Render Profile page & get gravatar icon
+exports.profile = function(req, res) {
+    // List all Users and sort by Date
+    res.render('profile', { title: 'Profile Page', user : req.user, avatar: gravatar.url(req.user.email ,  {s: '100', r: 'x', d: 'retro'}, true) });
 };
 
 
